5ab355d6eec541a62d751b5b081b8e536cd5b7b0
DavidRueda <davidrueda322442@correo.itm.edu.co>
2025-03-20 00:31:19 -0500

    Repuesto terminado casi

--- MetodosRepuesto.java
+++ MetodosRepuesto.java
@@ -4,17 +4,14 @@ import java.util.Stack;
 import javax.print.attribute.standard.JobHoldUntil;
 import javax.swing.JOptionPane;
 
-public class MetodosRepuesto {  
+public class MetodosRepuesto {
 
+    private Stack<ObjRepuesto> pila = new Stack<>();
     Scanner sc = new Scanner(System.in);
 
-    public void LlenarStock()
-    {
-        //Creo pila
-        Stack<ObjRepuesto> pila = new Stack<>();
+    public void LlenarStock() {
+        // Creo pila
         boolean bandera = true;
-        String opt = "";
-
         while (bandera) {
             ObjRepuesto obj = new ObjRepuesto();
 
@@ -25,85 +22,136 @@ public class MetodosRepuesto {
 
             pila.push(obj);
 
-            opt = JOptionPane.showInputDialog("¿Desea ingresar mas registros? SI/NO");
-            if(opt.equalsIgnoreCase("NO"))
-            {
+            String opt = JOptionPane.showInputDialog("¿Desea ingresar mas registros? SI/NO");
+            if (opt.equalsIgnoreCase("NO")) {
                 bandera = false;
             }
         }
-        MostrarStock(pila);
-        
+        Mostrar();
+
     }
 
-    public static void MostrarStock(Stack <ObjRepuesto> pila)
-        {
-            for (ObjRepuesto item : pila) {
-                System.out.println("Referencia: " + item.getReferencia());
-                System.out.println("Marca : " + item.getMarca());
-                System.out.println("Cantidad: " + item.getCantidad());
-                System.out.println("Precio: " + item.getPrecio());
-                System.out.println("--------------");
-                
+    public void Mostrar() {
+        if (pila.isEmpty()) {
+            JOptionPane.showMessageDialog(null, "El inventario está vacío");
+            return;
+        }
+
+        for (ObjRepuesto item : pila) {
+            System.out.println("Referencia: " + item.getReferencia());
+            System.out.println("Marca : " + item.getMarca());
+            System.out.println("Cantidad: " + item.getCantidad());
+            System.out.println("Precio: " + item.getPrecio());
+            System.out.println("--------------");
+        }
+    }
+
+    public void Buscar() {
+        String ref = JOptionPane.showInputDialog("Ingrese la referencia que desea buscar: ");
+        for (ObjRepuesto item : pila) {
+            if (item.getReferencia().equalsIgnoreCase(ref)) {
+                JOptionPane.showMessageDialog(null, "Repuesto encontrado\n" +
+                        "Marca: " + item.getMarca() + "\nCantidad: " + item.getCantidad() +
+                        "\nPrecio: " + item.getPrecio());
+                return;
             }
         }
+        JOptionPane.showMessageDialog(null, "Repuesto no encontrado");
+    }
 
-        public void Menu()
-        {   
-            String Men = "";
-            int opc = Integer.parseInt(JOptionPane.showInputDialog("INVENTARIO\n" + 
-                                    "1. Ingresar registros\n" +
-                                    "2. Buscar Registro\n" + 
-                                    "3. Modificar Registro\n" + 
-                                    "4. Eliminar Registro\n" + 
-                                    "5. Salir"));
-        while(opc != 1 && opc != 2 && opc != 3 && opc != 4 && opc != 5)
-        {
-            opc = Integer.parseInt(JOptionPane.showInputDialog("Dato Erroneo, intentelo de nuevo: "));
-        }
-        }
+    public void Modificar() {
+        String ref = JOptionPane.showInputDialog("Ingrese la referencia que desea modificar");
+        for (ObjRepuesto item : pila) {
+            if (item.getReferencia().equalsIgnoreCase(ref)) {
+                boolean continuar = true;
+                while (continuar) {
+                    int opcion = Integer.parseInt(JOptionPane.showInputDialog("¿Que campo desea modificar?\n" +
+                            "1. Referencia\n" +
+                            "2. Marca \n" +
+                            "3. Cantidad \n" +
+                            "4. Precio \n" +
+                            "5. Salir"));
 
-        public Stack<ObjRepuesto> Acciones(Stack<ObjRepuesto> pila, int opc)
-        {
-            String dato = "";
-
-            if(opc == 1)
-            {
-             LlenarStock();   
-            }else if(opc == 2)
-            {
-                dato = JOptionPane.showInputDialog("Buscar dato referncia: ");
-
-            }else if(opc == 3)
-            {
-                dato = JOptionPane.showInputDialog("Que referencia desea modificar: ");
-            }else if(opc == 4)
-            {
-                dato = JOptionPane.showInputDialog("Que referencia desea eliminar:");
-            }else
-            {
-                JOptionPane.showInputDialog("Hasta luego");
-            }
-
-
-            for (ObjRepuesto item : pila) {
-                if(item.getReferencia().equalsIgnoreCase(dato))
-                {
-                    if(opc == 2)
-                    {   
-                        int x = Integer.parseInt(JOptionPane.showInputDialog("¿Que campo desea modificar?"+ 
-                            "1. Referencia\n" + 
-                            "2. Marca\n" + 
-                            "3. Cantidad \n" + 
-                            "4. Precio \n"));
-                        while( x!=1 && x!=2 && x!=3 && x!=4)
-                        {
-                            x = Integer.parseInt(JOptionPane.showInputDialog("Digite un valor correcto"));
-                        }
-                            
+                    switch (opcion) {
+                        case 1:
+                            item.setReferencia(JOptionPane.showInputDialog("Nueva referencia: "));
+                            break;
+                        case 2:
+                            item.setMarca(JOptionPane.showInputDialog("Nueva Marca: "));
+                            break;
+                        case 3:
+                            item.setCantidad(Integer.parseInt(JOptionPane.showInputDialog("Nueva Cantidad: ")));
+                            break;
+                        case 4:
+                            item.setPrecio(Double.parseDouble(JOptionPane.showInputDialog("Nuevo Precio: ")));
+                            break;
+                        case 5:
+                            continuar = false;
+                            break;
+                        default:
+                            JOptionPane.showMessageDialog(null, "Opción no valida");
                     }
                 }
-                
+                JOptionPane.showMessageDialog(null, "Repuesto modificado con éxito");
             }
-            return pila;
         }
+        JOptionPane.showMessageDialog(null, "Repuesto no encontrado");
+    }
+
+    public void Eliminar() {
+        String ref = JOptionPane.showInputDialog("Ingrese la referencia del repuesto a eliminar: ");
+        boolean eliminado = pila.removeIf(item -> item.getReferencia().equalsIgnoreCase(ref));
+
+        if (eliminado) {
+            JOptionPane.showMessageDialog(null, "Repuesto eliminado con éxito.");
+        } else {
+            JOptionPane.showMessageDialog(null, "Repuesto no encontrado.");
+        }
+    }
+
+    public void Menu() {
+        int opc = 0;
+        boolean valido;
+        do {
+            do {
+                try {
+                    opc = Integer.parseInt(JOptionPane.showInputDialog("INVENTARIO\n" +
+                            "1. Ingresar registros\n" +
+                            "2. Buscar Registro\n" +
+                            "3. Modificar Registro\n" +
+                            "4. Eliminar Registro\n" +
+                            "5. Mostrar Inventario\n" +
+                            "6. Salir"));
+                    valido = (opc >= 1 && opc <= 6);
+                    if (!valido) {
+                        JOptionPane.showMessageDialog(null, "Opción no válida, intente de nuevo.");
+                    }
+                } catch (NumberFormatException e) {
+                    JOptionPane.showMessageDialog(null, "Entrada inválida, ingrese un número válido.");
+                    valido = false;
+                }
+            } while (!valido);
+
+            switch (opc) {
+                case 1:
+                    LlenarStock();
+                    break;
+                case 2:
+                    Buscar();
+                    break;
+                case 3:
+                    Modificar();
+                    break;
+                case 4:
+                    Eliminar();
+                    break;
+                case 5:
+                    Mostrar();
+                    break;
+                case 6:
+                    JOptionPane.showMessageDialog(null, "Hasta luego");
+                    break;
+            }
+        } while (opc != 6);
+    }
 }
